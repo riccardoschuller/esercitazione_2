b9532afa4f6cd78bb3ed8d7ab5adcf754df1ba5e
riccardoschuller <cartuciolo@hotmail.it>
2015-12-07 11:49:27 +0100

    add prompt js

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 var stringa ="gatti"
 
+
+
 console.log(stringa)
+
+
+var i = prompt("Inserisci un numero", "Digita un numero qui...") ;
+
+
+
+	
+	
+
+	 if (i%2 == 0) {
+	 	if (i != 4) {alert(i + " è pari")
+	};
+		
+	}
+
+	else if (i == 17|| i == 13) {
+		alert(i + " è un numero speciale!!!")
+	}
+
+	else if (isNaN(i) == true) {
+		alert(i + " non è un numero")
+	}
+
+	else {
+		alert(i + " è dispari")
+	}
+
+
+
